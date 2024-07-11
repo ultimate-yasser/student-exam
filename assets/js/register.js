8c06526f55798c7bbb37c7e0f54f2851b10d2c23
ultimate-yasser <ultimateyasser@gmail.com>
2024-07-11 16:37:57 +0300

    made login.html and put it into index.html action. finished almost most of the validation

--- assets/js/register.js
+++ assets/js/register.js
@@ -40,17 +40,86 @@ function validateForm(form){
     email = form["email"]
     password = form["password"]
     cpassword = form["cpassword"]
+    isValid = true
+
 
     // Validate First Name
     if (checkName(fname.value)){
         fname.style.border= "#28a745 3px solid"
+        document.getElementById("fname-error").textContent = ""
     }else{
         fname.style.border= "#dc3545 3px solid"
-        return false;
+        document.getElementById("fname-error").textContent = "First letter must be uppercase\
+        and must contain only letters"
+        isValid = false
+    }
+
+    // Validate Last Name
+    if (checkName(lname.value)){
+        lname.style.border= "#28a745 3px solid"
+        document.getElementById("lname-error").textContent = ""
+    }else{
+        lname.style.border= "#dc3545 3px solid"
+        document.getElementById("lname-error").textContent = "First letter must be uppercase\
+        and must contain only letters"
+        isValid = false
+    }
+
+    // Validate Username
+    if (checkUsername(username.value)){
+        username.style.border= "#28a745 3px solid"
+        document.getElementById("username-error").textContent = ""
+    }else{
+        username.style.border= "#dc3545 3px solid"
+        document.getElementById("username-error").textContent = "Shall Not contain\
+        space or special charchters except - and _"
+        isValid = false
+    }
+
+    // Validate Account Type
+    if (accountType.value){
+        accountType.style.border= "#28a745 3px solid"
+        document.getElementById("accountType-error").textContent = ""
+    }else{
+        document.getElementById("accountType-error").textContent = "This field is required"
+        isValid = false
+    }
+
+    // Validate Email
+    if (checkEmail(email.value)){
+        email.style.border= "#28a745 3px solid"
+        document.getElementById("email-error").textContent = ""
+    }else{
+        email.style.border= "#dc3545 3px solid"
+        document.getElementById("email-error").textContent = "We accept only\
+        gmail emails"
+        isValid = false
+    }
+
+    // Validate Password
+    if (checkPass(password.value)){
+        password.style.border= "#28a745 3px solid"
+        document.getElementById("password-error").textContent = ""
+    }else{
+        password.style.border= "#dc3545 3px solid"
+        document.getElementById("password-error").textContent = "Password length\
+        is from 8-16 and shall contain lowercase, uppercase, numbers and special charcters"
+        isValid = false
+    }
+
+    // Validate Password
+    if (checkConfPass(password, cpassword)){
+        cpassword.style.border= "#28a745 3px solid"
+        document.getElementById("cpassword-error").textContent = ""
+    }else{
+        cpassword.style.border= "#dc3545 3px solid"
+        document.getElementById("cpassword-error").textContent = "password is not\
+        the same as confirm password"
+        return false
     }
 
 
-    return false; // to make it never submit
+    return isValid; // to make it never submit
 }
 
 // Define the onsubmit function of the form
